ce711caf6b632d0f0f4e0bec7b51beb1cd9d9c8d
David Dong <daviddong201712@gmail.com>
2020-05-24 23:26:58 -0500

    chapter 11.5 is finished. This project is finished.

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloSpringController {
@@ -20,23 +24,38 @@ public class HelloSpringController {
     }
 
     //hello1?name=name
-    @ResponseBody
     @RequestMapping(value = "hello1", method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParameter(@RequestParam String name) {
-        return "Hello, " + name + " !";
+    public String helloWithQueryParameter(@RequestParam String name, Model model) {
+        String theGreeting = "hello " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
     //hello2/name
     @GetMapping("hello2/{name}")
-    @ResponseBody
-    public String helloWithPathParameter(@PathVariable String name) {
-        return "Hello, " + name + " !";
+    public String helloWithPathParameter(@PathVariable String name, Model model) {
+        String theGreeting = "hello " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
     //form
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-list")
+    public String helloNameList(Model model) {
+        List<String> nameList = new ArrayList<>();
+        nameList.add("NiCole");
+        nameList.add("Ethan");
+        nameList.add("Liano");
+        nameList.add("Damian");
+        nameList.add("Miley");
+        nameList.add("Aaron");
+        model.addAttribute("names",nameList);
+        return "hello-list";
     }
 
 }
